c00e94239f92b05555b50ae5995e42576285f191
Kazunori Kimura <kazunori.kimura.js@gmail.com>
2022-05-11 11:41:15 +0900

    [mod] API のデプロイ

---
+++ server/appping.sh
@@ -0,0 +1,1 @@
+curl -H "Origin: http://localhost:3000" -H "Access-Control-Request-Method: GET" -H "Access-Control-Request-Headers: Content-Type, Authorization, Xent-Type, Authorization, X-Requested-With" -X GET -v "https://shopping-list-dev.azurewebsites.net/thing"
--- server/prisma/migrations/20220509025426_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Thing" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "boughtAt" TEXT,
-    "createdAt" TEXT NOT NULL,
-    "updatedAt" TEXT NOT NULL
-);
