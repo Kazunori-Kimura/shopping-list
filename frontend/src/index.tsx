ef195772b02c1c68a7ae11afc9b142af75a34d6b
Kazunori Kimura <kazunori.kimura.js@gmail.com>
2022-05-11 11:46:18 +0900

    [mod] ServiceWorker を有効化

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,7 +14,18 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register({
+    onUpdate: (registration) => {
+        // アップデートが存在する
+        if (window.confirm('新しいアップデートが存在します。再読み込みしますか？')) {
+            if (registration && registration.waiting) {
+                registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+            }
+            // ブラウザの再読み込み
+            window.location.reload();
+        }
+    },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
